4a104437009f080a1997162347bd4cfed66ac622
okside32 <okside32@gmail.com>
2017-07-06 15:21:49 +0300

    first

---
+++ Report/Form1.Designer.cs
@@ -0,0 +1,258 @@
+﻿namespace Report
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.OpenForTopsButton = new System.Windows.Forms.Button();
+            this.sales = new System.Windows.Forms.DataGridView();
+            this.YesterdaySalesButton = new System.Windows.Forms.Button();
+            this.TodaySalesButton = new System.Windows.Forms.Button();
+            this.TopSalesPath = new System.Windows.Forms.TextBox();
+            this.YesterdaySalesPath = new System.Windows.Forms.TextBox();
+            this.TodaySalesPath = new System.Windows.Forms.TextBox();
+            this.SaveTableButton = new System.Windows.Forms.Button();
+            this.Tab = new System.Windows.Forms.TabControl();
+            this.tabTop = new System.Windows.Forms.TabPage();
+            this.tabRemains = new System.Windows.Forms.TabPage();
+            this.tabAudit = new System.Windows.Forms.TabPage();
+            this.RevisionTextBox = new System.Windows.Forms.TextBox();
+            this.RemainTextBox = new System.Windows.Forms.TextBox();
+            this.RevisionButton = new System.Windows.Forms.Button();
+            this.RemainButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sales)).BeginInit();
+            this.Tab.SuspendLayout();
+            this.tabTop.SuspendLayout();
+            this.tabRemains.SuspendLayout();
+            this.tabAudit.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // OpenForTopsButton
+            // 
+            this.OpenForTopsButton.Location = new System.Drawing.Point(3, 21);
+            this.OpenForTopsButton.Name = "OpenForTopsButton";
+            this.OpenForTopsButton.Size = new System.Drawing.Size(75, 23);
+            this.OpenForTopsButton.TabIndex = 0;
+            this.OpenForTopsButton.Text = "Top";
+            this.OpenForTopsButton.UseVisualStyleBackColor = true;
+            this.OpenForTopsButton.Click += new System.EventHandler(this.Open_Click);
+            // 
+            // sales
+            // 
+            this.sales.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.sales.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.sales.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.sales.Location = new System.Drawing.Point(8, 106);
+            this.sales.Name = "sales";
+            this.sales.Size = new System.Drawing.Size(585, 448);
+            this.sales.TabIndex = 1;
+            this.sales.DataBindingComplete += new System.Windows.Forms.DataGridViewBindingCompleteEventHandler(this.sales_DataBindingComplete);
+            // 
+            // YesterdaySalesButton
+            // 
+            this.YesterdaySalesButton.Location = new System.Drawing.Point(3, 6);
+            this.YesterdaySalesButton.Name = "YesterdaySalesButton";
+            this.YesterdaySalesButton.Size = new System.Drawing.Size(75, 23);
+            this.YesterdaySalesButton.TabIndex = 2;
+            this.YesterdaySalesButton.Text = "First";
+            this.YesterdaySalesButton.UseVisualStyleBackColor = true;
+            this.YesterdaySalesButton.Click += new System.EventHandler(this.YesterdaySales_Click);
+            // 
+            // TodaySalesButton
+            // 
+            this.TodaySalesButton.Location = new System.Drawing.Point(3, 36);
+            this.TodaySalesButton.Name = "TodaySalesButton";
+            this.TodaySalesButton.Size = new System.Drawing.Size(75, 23);
+            this.TodaySalesButton.TabIndex = 3;
+            this.TodaySalesButton.Text = "Second";
+            this.TodaySalesButton.UseVisualStyleBackColor = true;
+            this.TodaySalesButton.Click += new System.EventHandler(this.TodaySales_Click);
+            // 
+            // TopSalesPath
+            // 
+            this.TopSalesPath.Location = new System.Drawing.Point(84, 23);
+            this.TopSalesPath.Name = "TopSalesPath";
+            this.TopSalesPath.Size = new System.Drawing.Size(488, 20);
+            this.TopSalesPath.TabIndex = 4;
+            // 
+            // YesterdaySalesPath
+            // 
+            this.YesterdaySalesPath.Location = new System.Drawing.Point(84, 6);
+            this.YesterdaySalesPath.Name = "YesterdaySalesPath";
+            this.YesterdaySalesPath.Size = new System.Drawing.Size(487, 20);
+            this.YesterdaySalesPath.TabIndex = 5;
+            // 
+            // TodaySalesPath
+            // 
+            this.TodaySalesPath.Location = new System.Drawing.Point(84, 39);
+            this.TodaySalesPath.Name = "TodaySalesPath";
+            this.TodaySalesPath.Size = new System.Drawing.Size(487, 20);
+            this.TodaySalesPath.TabIndex = 6;
+            // 
+            // SaveTableButton
+            // 
+            this.SaveTableButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.SaveTableButton.Location = new System.Drawing.Point(8, 560);
+            this.SaveTableButton.Name = "SaveTableButton";
+            this.SaveTableButton.Size = new System.Drawing.Size(75, 23);
+            this.SaveTableButton.TabIndex = 7;
+            this.SaveTableButton.Text = "Save";
+            this.SaveTableButton.UseVisualStyleBackColor = true;
+            this.SaveTableButton.Click += new System.EventHandler(this.SaveTable_Click);
+            // 
+            // Tab
+            // 
+            this.Tab.AllowDrop = true;
+            this.Tab.Controls.Add(this.tabTop);
+            this.Tab.Controls.Add(this.tabRemains);
+            this.Tab.Controls.Add(this.tabAudit);
+            this.Tab.Location = new System.Drawing.Point(1, 1);
+            this.Tab.Multiline = true;
+            this.Tab.Name = "Tab";
+            this.Tab.SelectedIndex = 0;
+            this.Tab.Size = new System.Drawing.Size(592, 99);
+            this.Tab.TabIndex = 8;
+            // 
+            // tabTop
+            // 
+            this.tabTop.Controls.Add(this.OpenForTopsButton);
+            this.tabTop.Controls.Add(this.TopSalesPath);
+            this.tabTop.Location = new System.Drawing.Point(4, 22);
+            this.tabTop.Name = "tabTop";
+            this.tabTop.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTop.Size = new System.Drawing.Size(584, 73);
+            this.tabTop.TabIndex = 0;
+            this.tabTop.Text = "Топ";
+            this.tabTop.UseVisualStyleBackColor = true;
+            // 
+            // tabRemains
+            // 
+            this.tabRemains.Controls.Add(this.YesterdaySalesButton);
+            this.tabRemains.Controls.Add(this.TodaySalesButton);
+            this.tabRemains.Controls.Add(this.TodaySalesPath);
+            this.tabRemains.Controls.Add(this.YesterdaySalesPath);
+            this.tabRemains.Location = new System.Drawing.Point(4, 22);
+            this.tabRemains.Name = "tabRemains";
+            this.tabRemains.Padding = new System.Windows.Forms.Padding(3);
+            this.tabRemains.Size = new System.Drawing.Size(584, 73);
+            this.tabRemains.TabIndex = 1;
+            this.tabRemains.Text = "Остатки";
+            this.tabRemains.UseVisualStyleBackColor = true;
+            // 
+            // tabAudit
+            // 
+            this.tabAudit.Controls.Add(this.RevisionTextBox);
+            this.tabAudit.Controls.Add(this.RemainTextBox);
+            this.tabAudit.Controls.Add(this.RevisionButton);
+            this.tabAudit.Controls.Add(this.RemainButton);
+            this.tabAudit.Location = new System.Drawing.Point(4, 22);
+            this.tabAudit.Name = "tabAudit";
+            this.tabAudit.Padding = new System.Windows.Forms.Padding(3);
+            this.tabAudit.Size = new System.Drawing.Size(584, 73);
+            this.tabAudit.TabIndex = 2;
+            this.tabAudit.Text = "Переоценка";
+            this.tabAudit.UseVisualStyleBackColor = true;
+            // 
+            // RevisionTextBox
+            // 
+            this.RevisionTextBox.Location = new System.Drawing.Point(100, 35);
+            this.RevisionTextBox.Name = "RevisionTextBox";
+            this.RevisionTextBox.Size = new System.Drawing.Size(478, 20);
+            this.RevisionTextBox.TabIndex = 3;
+            // 
+            // RemainTextBox
+            // 
+            this.RemainTextBox.Location = new System.Drawing.Point(100, 9);
+            this.RemainTextBox.Name = "RemainTextBox";
+            this.RemainTextBox.Size = new System.Drawing.Size(478, 20);
+            this.RemainTextBox.TabIndex = 2;
+            // 
+            // RevisionButton
+            // 
+            this.RevisionButton.Location = new System.Drawing.Point(7, 35);
+            this.RevisionButton.Name = "RevisionButton";
+            this.RevisionButton.Size = new System.Drawing.Size(87, 23);
+            this.RevisionButton.TabIndex = 1;
+            this.RevisionButton.Text = "Переоценка";
+            this.RevisionButton.UseVisualStyleBackColor = true;
+            this.RevisionButton.Click += new System.EventHandler(this.RevisionButton_Click);
+            // 
+            // RemainButton
+            // 
+            this.RemainButton.Location = new System.Drawing.Point(7, 6);
+            this.RemainButton.Name = "RemainButton";
+            this.RemainButton.Size = new System.Drawing.Size(87, 23);
+            this.RemainButton.TabIndex = 0;
+            this.RemainButton.Text = "Остатки";
+            this.RemainButton.UseVisualStyleBackColor = true;
+            this.RemainButton.Click += new System.EventHandler(this.RemainButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(603, 588);
+            this.Controls.Add(this.Tab);
+            this.Controls.Add(this.SaveTableButton);
+            this.Controls.Add(this.sales);
+            this.Name = "Form1";
+            this.Text = "Отчеты";
+            ((System.ComponentModel.ISupportInitialize)(this.sales)).EndInit();
+            this.Tab.ResumeLayout(false);
+            this.tabTop.ResumeLayout(false);
+            this.tabTop.PerformLayout();
+            this.tabRemains.ResumeLayout(false);
+            this.tabRemains.PerformLayout();
+            this.tabAudit.ResumeLayout(false);
+            this.tabAudit.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button OpenForTopsButton;
+        private System.Windows.Forms.DataGridView sales;
+        private System.Windows.Forms.Button YesterdaySalesButton;
+        private System.Windows.Forms.Button TodaySalesButton;
+        private System.Windows.Forms.TextBox TopSalesPath;
+        private System.Windows.Forms.TextBox YesterdaySalesPath;
+        private System.Windows.Forms.TextBox TodaySalesPath;
+        private System.Windows.Forms.Button SaveTableButton;
+        private System.Windows.Forms.TabControl Tab;
+        private System.Windows.Forms.TabPage tabTop;
+        private System.Windows.Forms.TabPage tabRemains;
+        private System.Windows.Forms.TabPage tabAudit;
+        private System.Windows.Forms.TextBox RevisionTextBox;
+        private System.Windows.Forms.TextBox RemainTextBox;
+        private System.Windows.Forms.Button RevisionButton;
+        private System.Windows.Forms.Button RemainButton;
+    }
+}
+
---
+++ Report/Form1.cs
@@ -0,0 +1,336 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+using ClosedXML.Excel;
+using Excel;
+using GemBox.Spreadsheet;
+
+namespace Report
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private DataTable yesterdayDataTable;
+        private DataTable remainDataTable;
+        private DataTable revisionDataTable;
+        private DataTable todayDataTable;
+        private const int topCount = 6;
+        private List<string>filterArray = new List<string> { "чол", "жін" ,"підл", "дит","юн" };
+        private DataTable cleanData(DataTable dataTable)
+        {
+            dataTable.Columns.RemoveAt(0);
+            dataTable.Columns[0].ColumnName = "article";
+            dataTable.Columns[1].ColumnName = "initial";
+            dataTable.Columns[2].ColumnName = "receipts";
+            dataTable.Columns[3].ColumnName = "rate";
+            dataTable.Columns[4].ColumnName = "final";
+            dataTable.AcceptChanges();
+            DataTable resTable = dataTable.Clone();
+            for (int i = dataTable.Rows.Count - 1; i >= 0; i--)
+            {
+                DataRow dr = dataTable.Rows[i];
+                if (Regex.IsMatch(dr["article"].ToString(), @"^\d"))
+                {
+                    if (dr["rate"] == DBNull.Value)
+                    {
+                        dr["rate"] = 0.0;
+                    }
+                    if (dr["final"] == DBNull.Value)
+                    {
+                        dr["final"] = 0.0;
+                    }
+                    resTable.Rows.Add(dr.ItemArray);
+                    resTable.AcceptChanges();
+
+                }
+            }
+            return resTable;
+        }
+        private DataTable getTop(DataTable dt, string filter)
+        {
+            var searchTerm =
+           new Regex(@"\s" + filter + ".?");
+            //var res = (from d in dt.AsEnumerable()
+            //           from c in notContains
+            //           where !(d.Field<string>("article").Contains(c))
+            //           where (d.Field<string>("article").Contains(filter))
+            //           orderby d.Field<double>("rate") descending
+            //           select d).Distinct().Take(5);
+            var res = (from d in dt.AsEnumerable()
+                       let matches = searchTerm.Matches(d.Field<string>("article"))
+                       where matches.Count > 0
+                       orderby d.Field<double>("rate") descending
+                       select d
+                      ).Distinct().Take(topCount);
+            return res.CopyToDataTable();
+        }
+        private void colorizeDataView(int columnCount, int rowCount)
+        {
+            for (int j = 0; j < columnCount; j++)
+            {
+                for (int i = 0; i < rowCount - 1; i++)
+                {
+                    if ((double)(i / (topCount*1)) < 1.0)
+                    {
+                        sales.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Aqua;
+                        continue;
+                    }
+                    if ((double)(i / (topCount * 2)) < 1.0)
+                    {
+                        sales.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.DarkSalmon;
+                        continue;
+                    }
+                    if ((double)(i / (topCount * 3)) < 1.0)
+                    {
+                        sales.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.DarkKhaki;
+                        continue;
+                    }
+
+                    if ((double)(i / (topCount * 4)) < 1.0)
+                    {
+                        sales.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.ForestGreen;
+                        continue;
+                    }
+                    if ((double)(i / (topCount * 5)) < 1.0)
+                    {
+                        sales.Rows[i].Cells[j].Style.BackColor = System.Drawing.Color.Gold;
+                        continue;
+                    }
+                }
+            }
+        }
+        private DataTable excelToRemainTable(OpenFileDialog ofd)
+        {
+            FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateBinaryReader(fs);
+            reader.IsFirstRowAsColumnNames = false;
+            var table = reader.AsDataSet().Tables[0];
+            reader.Close();
+            return this.cleanData(table);
+        }
+        private DataTable cleanRevisionData(DataTable dataTable)
+        {
+            //dataTable.Columns.RemoveAt(0);
+            dataTable.Columns[0].ColumnName = "article";
+            dataTable.Columns[1].ColumnName = "initial";
+            dataTable.Columns[2].ColumnName = "receipts";
+            dataTable.Columns[3].ColumnName = "rate";
+            dataTable.Columns[4].ColumnName = "final";
+            dataTable.AcceptChanges();
+            DataTable resTable = dataTable.Clone();
+            for (int i = dataTable.Rows.Count - 1; i >= 0; i--)
+            {
+                DataRow dr = dataTable.Rows[i];
+                if (Regex.IsMatch(dr["article"].ToString(), @"^\d"))
+                {
+                    if (dr["rate"] == DBNull.Value)
+                    {
+                        dr["rate"] = 0.0;
+                    }
+                    if (dr["final"] == DBNull.Value)
+                    {
+                        dr["final"] = 0.0;
+                    }
+                    resTable.Rows.Add(dr.ItemArray);
+                    resTable.AcceptChanges();
+
+                }
+            }
+            return resTable;
+        }
+        private DataTable excelToRevisionTable(OpenFileDialog ofd)
+        {
+            FileStream fs = File.Open(ofd.FileName, FileMode.Open, FileAccess.Read);
+            IExcelDataReader reader = ExcelReaderFactory.CreateBinaryReader(fs);
+            reader.IsFirstRowAsColumnNames = false;
+            var table = reader.AsDataSet().Tables[0];
+            reader.Close();
+            return this.cleanRevisionData(table);
+        }
+        private DataTable getSingleReminsDataTable(DataTable dataTable)
+        {
+            return (
+                dataTable.AsEnumerable()
+                .Where(r => r.Field<double>("final")==1.0)
+                ).CopyToDataTable();
+        }
+        private DataTable FindRemains()
+        {
+            this.yesterdayDataTable = this.getSingleReminsDataTable(this.yesterdayDataTable);       
+            this.todayDataTable = this.getSingleReminsDataTable(this.todayDataTable);
+            DataTable TableC = this.yesterdayDataTable.AsEnumerable()
+            .Where(ra => this.todayDataTable.AsEnumerable()
+            .All(rb => rb.Field<string>("article") != ra.Field<string>("article"))
+        )
+    .CopyToDataTable();
+            return TableC;
+        }
+        private void SaveToExcel(DataTable dt,string fileName)
+        {
+            XLWorkbook wb = new XLWorkbook();
+            wb.Worksheets.Add(dt,"report");
+            wb.SaveAs(fileName);
+        }
+        private void Open_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        TopSalesPath.Text = ofd.FileName;
+                        var table = this.excelToRemainTable(ofd);
+                        var resDataTable = table.Clone();
+                        foreach (var filter in this.filterArray)
+                        {
+                            resDataTable.Merge(this.getTop(table, filter));
+                        }
+                        sales.DataSource = resDataTable;
+                        //this.sales.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCellsExceptHeader;
+                        this.colorizeDataView(sales.ColumnCount, sales.RowCount);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+
+           
+        }
+        private void YesterdaySales_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        YesterdaySalesPath.Text = ofd.FileName;
+                        this.yesterdayDataTable = this.excelToRemainTable(ofd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            
+        }
+        private void TodaySales_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        TodaySalesPath.Text = ofd.FileName;
+                        this.todayDataTable = this.excelToRemainTable(ofd);
+                        sales.DataSource = this.FindRemains();
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void SaveTable_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SaveFileDialog saveFileDialog = new SaveFileDialog();
+                saveFileDialog.SupportMultiDottedExtensions = false;
+                saveFileDialog.ValidateNames = true;
+                saveFileDialog.DereferenceLinks = false; // Will return .lnk in shortcuts.
+                saveFileDialog.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+                saveFileDialog.ShowDialog();
+                if (saveFileDialog.FileName != "")
+                {
+                    string newFileName = saveFileDialog.FileName;
+                    if (!newFileName.Contains(".xlsx"))
+                    {
+                        newFileName = newFileName + ".xlsx";
+                    }
+                    var dt = (DataTable)(sales.DataSource);
+                    this.SaveToExcel(dt, newFileName);
+                    MessageBox.Show("Saved " + newFileName);
+                }
+            }
+            catch (Exception exception)
+            {
+                MessageBox.Show(exception.Message, "Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+            
+        }
+
+        private void RemainButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        RemainTextBox.Text = ofd.FileName;
+                        this.remainDataTable = this.excelToRemainTable(ofd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+        }
+
+        private void RevisionButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                using (OpenFileDialog ofd = new OpenFileDialog())
+                {
+                    if (ofd.ShowDialog() == DialogResult.OK)
+                    {
+                        RevisionTextBox.Text = ofd.FileName;
+                        this.revisionDataTable = this.excelToRevisionTable(ofd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message, "Error",
+                                 MessageBoxButtons.OK,
+                                 MessageBoxIcon.Error);
+            }
+        }
+
+        private void sales_DataBindingComplete(object sender, DataGridViewBindingCompleteEventArgs e)
+        {
+            this.sales.Columns[0].AutoSizeMode = DataGridViewAutoSizeColumnMode.DisplayedCellsExceptHeader;
+        }
+    }
+}
